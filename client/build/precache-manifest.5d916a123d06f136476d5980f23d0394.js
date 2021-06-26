140a05dfe0f2c18a1f977f0a354e87015b760c56
EricMalpass <ericmalpass@gmail.com>
2021-06-25 18:51:25 -0700

    test

---
+++ client/build/precache-manifest.5d916a123d06f136476d5980f23d0394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175a369b0c485f1054295915265242a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41a387a1d1a035ff90c",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "563812fdcfa95d36e049",
+    "url": "/static/js/2.dbbf1802.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.dbbf1802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41a387a1d1a035ff90c",
+    "url": "/static/js/main.2d6e8ce0.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
